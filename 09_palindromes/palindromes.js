741ba1208f21f1f99f0b9e2aee68fcdc61e6a22b
Ethan Hearn <hearn@hearn.nomads.utk.edu>
2022-03-10 10:27:15 -0500

    exercises 9-11 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringArray = [];
+
+    // Remove non letter characters and uppercase
+    string = string.replace(/\W/g, '').toLowerCase()
+    
+
+    // Add each letter to beginning of array
+    for (let i=0; i<string.length; i++) {
+        stringArray.unshift(string[i]);
+    }
+
+    // Convert array to string
+    const backwards = stringArray.join('');
+
+    // Check for equality
+    return backwards == string;
 
 };
 
